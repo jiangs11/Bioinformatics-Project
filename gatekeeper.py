19873d286ca21c49efb864a20807856a7fe3346a
Steven Jiang <jiangsteven99@gmail.com>
2021-10-21 16:55:02 -0400

    changed processing to first 1000 unique sequences

--- gatekeeper.py
+++ gatekeeper.py
@@ -4,9 +4,50 @@
 # Class: Graduate Bioinformatics, Dr. Chen
 
 import os
+from Bio import SeqIO
 
-def runGATEkeeper():
-    return
+fileName = 'top1000UniqueSequences.fasta'
+tempFile1 = 'tempFile1.fasta'
+tempFile2 = 'tempFile2.fasta'
+outputFile = 'output'
+outputFolder = 'gatekeeperOutput'
+
+# Set up command to run GATEkeeper
+def runGATEkeeper(counter):
+    command = './GATEkeeper/bin/GATEkeeper -r ' + tempFile1 + ' -q ' + tempFile2 + ' -o ' + outputFile + str(counter).zfill(6)
+    os.system(command)
+
+# Moves all output.maf and output.vcf files to subdirectory and deletes other GATEkeeper generated files
+def moveDeleteFiles():
+    os.system('mkdir ' + outputFolder)
+    os.system('mv output*.* ' + outputFolder)
+
+    os.system('rm tempFile*.*')
+    os.system('rm tempFile*.*')
+
+def main():
+    counter = 0
+
+    with open(fileName) as handle:
+        for record in SeqIO.parse(handle, 'fasta'):
+            print('***** Current Index *****:', counter)
+            description, sequence = record.description, str(record.seq)
+
+            # This is the Wuhan Genome with ID NC_045512.2
+            if counter == 0:
+                with open(tempFile1, 'w') as tempHandle1:
+                    tempHandle1.write('>' + description + '\n')
+                    tempHandle1.write(sequence + '\n')
+            else:
+                with open(tempFile2, 'w') as tempHandle2:
+                    tempHandle2.write('>' + description + '\n')
+                    tempHandle2.write(sequence + '\n')
+
+            runGATEkeeper(counter)
+            counter += 1
+
+    moveDeleteFiles()
+
 
 if __name__ == '__main__':
-    runGATEkeeper()
+    main()
--- preprocessing.py
+++ preprocessing.py
@@ -8,9 +8,13 @@ from Bio import SeqIO
 # Stores the genome sequences as keys, and times that sequence occurs and its description as values
 data = {}
 
-rawInputFile = 'sequences.fasta'
-uniqueOutputFile = 'uniqueSequences.fasta'
+numUnique = 1000
 
+rawInputFile = 'sequences.fasta'
+allUniqueOutputFile = 'allUniqueSequences.fasta'
+topNUniqueOutputFile = 'top' + str(numUnique) + 'UniqueSequences.fasta'
+
+# Takes raw fasta file and stores them into dictionary
 def readProcessData():
     totalNumGenomesDownloaded = 0
 
@@ -30,6 +34,7 @@ def readProcessData():
 
     return totalNumGenomesDownloaded
 
+# Deletes all genome records that contain an 'N' in its sequence
 def processGenomes():
     dataWithoutN = data.copy()
 
@@ -39,19 +44,33 @@ def processGenomes():
 
     return dataWithoutN
 
+# Writes all unique sequences to one file and the top N (1000 for this project) unique sequences to another file
 def writeUniqueGenomesToFile(finalData):
-    with open(uniqueOutputFile, 'a') as file:
+    with open(allUniqueOutputFile, 'a') as file:
         for i in finalData:
             file.write('>' + finalData[i]['description'][0] + '\n')
             file.write(i + '\n')
 
+    with open(topNUniqueOutputFile, 'a') as file:
+        counter = 0
+        for i in finalData:
+            file.write('>' + finalData[i]['description'][0] + '\n')
+            file.write(i + '\n')
+            counter += 1
+
+            if counter == numUnique:
+                break
+
+        return counter
+
 def main():
     totalNumGenomesDownloaded = readProcessData()
     genomesWithoutN = processGenomes()
-    writeUniqueGenomesToFile(genomesWithoutN)
+    topNWritten = writeUniqueGenomesToFile(genomesWithoutN)
 
     print('Total number of genomes downloaded:', totalNumGenomesDownloaded)
-    print('Final number of unique genomes:', len(genomesWithoutN))
+    print('Total number of unique genomes:', len(genomesWithoutN))
+    print('Total N unique genomes:', topNWritten)
 
 if __name__ == '__main__':
     main()
